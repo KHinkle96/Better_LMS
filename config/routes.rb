71c04fe93ffcbc4e9efb5203734aceacac55beae
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-18 10:48:39 -0800

    Created pages controller and a home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :grades
   resources :assignments
   resources :courses
+  root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
